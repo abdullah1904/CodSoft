6f943d7e96d7e96226b5b1f46e7a212ba5f9b464
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2023-08-29 09:48:43 +0500

    Todo List

--- Level 1/Todo List/script.js
+++ Level 1/Todo List/script.js
@@ -1,7 +1,78 @@
-var tasks = document.querySelectorAll(".tasks");
-console.log(tasks);
-for(let task of tasks){
-    task.addEventListener("click", ()=>{
-        task.value = "Abdullah";
+var task = document.getElementById("task");
+var button = document.getElementById("btn");
+var tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+
+var del = ()=>{
+    document.querySelectorAll(".remove").forEach((d,i)=>{
+        d.addEventListener("click",()=>{
+            tasks.splice(i,1);
+            localStorage.setItem("tasks",JSON.stringify(tasks));
+            location.reload();
+        })
     })
 }
+
+var edit = ()=>{
+    let updateBtn = document.querySelectorAll(".updater");
+    let taskBox = document.querySelectorAll(".inputSetter textarea");
+    document.querySelectorAll(".edit").forEach((e,i)=>{
+        e.addEventListener("click",()=>{
+            updateBtn[i].style.display = "inline-block";
+            taskBox[i].disabled = false
+        })
+    })
+}
+
+var save = ()=>{
+    let taskBox = document.querySelectorAll(".inputSetter textarea");
+    document.querySelectorAll(".save").forEach((s,i)=>{
+        s.addEventListener("click",()=>{
+            tasks[i] = taskBox[i].value;
+            localStorage.setItem("tasks",JSON.stringify(tasks));
+            location.reload();
+        })
+    })
+}
+
+var cancel = ()=>{
+    document.querySelectorAll(".cancel").forEach((c)=>{
+        c.addEventListener("click",()=>{
+            location.reload();
+        })
+    })
+}
+
+var display = ()=>{
+    let list = "";
+    for(let i =0 ; i<tasks.length;i++){
+        list += `<section class="item">
+                    <section class="inputSetter">
+                        <textarea disabled>${tasks[i]}</textarea>
+                        <section class="editBtns">
+                            <button class="remove">Remove</button>
+                            <button class="edit">Edit</button>
+                        </section>
+                    </section>
+                    <section class="updater">
+                        <button class="save">Save</button>
+                        <button class="cancel">Cancel</button>
+                    </section>
+                </section>`
+    }
+    document.querySelector(".tasks-box").innerHTML = list;
+}
+
+button.addEventListener("click",()=>{
+    tasks.push(task.value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    task.value = "";
+    location.reload();
+});
+
+window.addEventListener("load",()=>{
+    display();
+    del();
+    edit();
+    save();
+    cancel();
+})
