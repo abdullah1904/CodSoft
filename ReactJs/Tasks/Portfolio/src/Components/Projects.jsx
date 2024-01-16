147a106034faf44c9c2facf8813f909c0eead314
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2024-01-16 16:45:19 +0500

    Portfolio

---
+++ ReactJs/Tasks/Portfolio/src/Components/Projects.jsx
@@ -0,0 +1,47 @@
+export default function Projects() {
+    return (
+        <div className="bg-white text-one py-5">
+            <div className="w-80 mx-auto">
+                <h2 className="text-center text-5xl font-one font-semibold border-one border-b-4 border-double py-2">Projects</h2>
+            </div>
+            <div className="grid md:grid-cols-3 gap-4 px-6 md:px-10 py-10 md:py-5">
+                <div className="card glass">
+                    <div className="card-body">
+                        <h2 className="card-title">Landing Page of Fast Food Court</h2>
+                        <p>Landing page using React, Bootstrap and React Router</p>
+                    </div>
+                </div>
+                <div className="card glass">
+                    <div className="card-body">
+                        <h2 className="card-title">Map Navigation System</h2>
+                        <p>Navigation optimization using C++, Object-Oriented Programming, Data Structures, Graph Theory and Algorithms</p>
+                    </div>
+                </div>
+                <div className="card glass">
+                    <div className="card-body">
+                        <h2 className="card-title">Result System using JavaScript and jQuery</h2>
+                        <p>Result system using Object-Oriented Programming JavaScript and jQuery</p>
+                    </div>
+                </div>
+                <div className="card glass">
+                    <div className="card-body">
+                        <h2 className="card-title">Showroom Management System</h2>
+                        <p>Data Management System using Object Oriented Programming in C++</p>
+                    </div>
+                </div>
+                <div className="card glass">
+                    <div className="card-body">
+                        <h2 className="card-title">Quiz App</h2>
+                        <p>Assessment System using Function and Procedural Programming in C++</p>
+                    </div>
+                </div>
+                <div className="card glass">
+                    <div className="card-body">
+                        <h2 className="card-title">Task Manager Application</h2>
+                        <p>CRUD Application using React, React Router and Bootstrap</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
